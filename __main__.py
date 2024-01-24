06bfa9dcf6ac74969ef38038b0d139efe5f19f7c
NikolaVukashinovich <nikola.vukasinovic@spotsie.io>
2024-01-24 09:48:10 +0100

    basic simple settings

--- __main__.py
+++ __main__.py
@@ -4,19 +4,18 @@ import pulumi
 import pulumi_azure as azure
 
 # Create an Azure Resource Group
-example_resource_group = azure.core.ResourceGroup("exampleResourceGroup", location="West Europe")
+example_resource_group = azure.core.ResourceGroup("example-resource-group", location="West Europe")
 
 # Create an Azure resource (Storage Account)
-flexible_server = azure.postgresql.FlexibleServer("exampleFlexibleServer",
+flexible_server = azure.postgresql.FlexibleServer("example-fs",
     resource_group_name=example_resource_group.name,
     location=example_resource_group.location,
     version="12",
     administrator_login="psqladmin",
     administrator_password="H@Sh1CoR3!",
-    public_network_access_enabled = True, 
     zone="2",
     storage_mb=32768,
-    sku_name="Standard_B1ms",
+    sku_name="B_Standard_B1ms",
 )
 
 pulumi.export("flexible_server", flexible_server)
